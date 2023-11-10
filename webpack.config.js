4b5856a45e3f157a3d1f23729a800a42783cf60c
Olivier Gorzalka <olivier@amphibee.fr>
2023-11-10 13:22:48 +0100

    Retrait babel dans webpack.config.js + ajout plugin aspect-ratio de TailwindCSS

--- webpack.config.js
+++ webpack.config.js
@@ -28,13 +28,6 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader', 'postcss-loader']
             },
